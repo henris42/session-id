85cf531183ddbd0454f9dc5620a99c5fe3f2257e
Henri Sundelin <henri.sundelin@iki.fi>
2019-08-13 23:52:42 +0300

    initial source

---
+++ src/lib.rs
@@ -0,0 +1,92 @@
+
+//use std::sync::*;
+//use std::cell::*;
+//use std::rc::*;
+
+use hmac::Hmac;
+//use log::*;
+use rand::Rng;
+use sha2::Sha256;
+
+pub struct State {
+    secret: Vec<u8>,
+    salt: Vec<u8>,
+    step: usize,
+    rounds: usize,
+}
+
+
+pub struct SessionId {
+    session: Box<State>,
+}
+impl SessionId {
+    pub fn new(rounds: usize, salt: Vec<u8>) -> SessionId {
+        let mut array = [0_u8; 32];
+        rand::thread_rng().fill(&mut array);
+        let s = Box::new(State {
+                secret: array.to_vec(),
+                salt: salt,
+                step: 0,
+                rounds: rounds,
+            });
+
+        SessionId {
+            session: s,
+        }
+    }
+
+    pub fn get(&self) -> Vec<u8> {
+        let mut v = [0_u8; 32];
+        pbkdf2::pbkdf2::<Hmac<Sha256>>(
+            &self.session.secret,
+            &self.session.salt,
+            self.session.rounds + self.session.step,
+            &mut v,
+        );
+        v.to_vec()
+    }
+
+    pub fn get_b64(&self) -> String {
+        let v = self.get();
+        let b64 = base64::encode(&v);
+        b64
+    }
+
+    pub fn next(&mut self) {
+        self.session.step = self.session.step + 1;
+        //&self
+    }
+
+    pub fn get_secret(&self) -> Vec<u8> {
+        self.session.secret.to_vec()
+    }
+
+    pub fn set_secret(&mut self, secret: Vec<u8>) {
+        self.session.secret = secret;
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        simple_logger::init().unwrap();
+        let salt = hex::decode("aaccee0022446688aaccee0022446688").unwrap();
+        let mut s = super::SessionId::new(1000, salt);
+
+        let ss = hex::decode("aaccee0022446688aaccee0022446688").unwrap();
+        s.set_secret(ss.to_vec());
+
+        log::info!("r1= {:x?}", &s.get());
+        log::info!("r1= {:x?}", &s.get());
+        s.next();
+        log::info!("r1= {:x?}", &s.get());
+        log::info!("r1= {:x?}", &s.get());
+        s.next();
+        log::info!("r1= {:x?}", &s.get());
+        log::info!("r1= {}", &s.get_b64());
+
+        assert_eq!(2 + 2, 4);
+    }
+}
